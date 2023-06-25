9cedd82d207ae76d2234e66305fe6cd258d03386
AritraGanguly <aaritra.ganguly@gmail.com>
2023-06-25 20:39:09 +0530

    Added Filter logic and Search name in Mock data

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -1,18 +1,26 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import JsonData from './MOCK_DATA.json';
 import './App.css'
 
 function App() {
+
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className='app'>
       <h1 className='app-heading'>Search Filter</h1>
       <div className='search-bar-container'>
-        <input type="text" placeholder='Search.....' />
-        {JsonData.map((value, map) => {
+        <input type="text" placeholder='Search.....' onChange={(event) => { setSearchValue(event.target.value) }} />
+        {JsonData.filter((val) => {
+          if (searchValue == '') return val;
+          else if (val.first_name.toLowerCase().includes(searchValue.toLowerCase())) {
+            return val
+          }
+        }).map((value, map) => {
           return (
             <div className='user'>
               <p>
-              {`${value.first_name} ${value.last_name}`}
+                {`${value.first_name} ${value.last_name}`}
               </p>
             </div>
           );
