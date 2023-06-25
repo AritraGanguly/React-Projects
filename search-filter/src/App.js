9a48a54d4793a74ae1d0db4d88a426c73efe64e9
AritraGanguly <aaritra.ganguly@gmail.com>
2023-06-25 20:20:37 +0530

    Added Search input and data in the app.js page

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React , {useState} from 'react';
+import JsonData from './MOCK_DATA.json';
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='app'>
+      <h1 className='app-heading'>Search Filter</h1>
+      <div className='search-bar-container'>
+        <input type="text" placeholder='Search.....' />
+        {JsonData.map((value, map) => {
+          return (
+            <div className='user'>
+              <p>
+              {`${value.first_name} ${value.last_name}`}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   )
 }
 
